05b824500541a2f2dd247165f2f29c3b09fc2825
mahsumurebe <info@mahsumurebe.com>
2021-10-17 02:39:43 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# dart-bitcoin-ops
+Bitcoin OP Codes.
+
+## Installation
+        dart pub add dart_bitcoin_ops
+
+## License [MIT](LICENSE)
---
+++ lib/dart_bitcoin_ops.dart
@@ -0,0 +1,6 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dart_bitcoin_ops;
+
+export 'src/dart_bitcoin_ops_base.dart';
